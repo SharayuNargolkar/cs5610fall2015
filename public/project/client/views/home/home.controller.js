557bcc87dfe6a4a906962f7b3c2a452eb5246f2b
SharayuNargolkar <nargolkar.s@husky.neu.edu>
2015-12-10 23:51:11 -0500

    changes to home page

--- public/project/client/views/home/home.controller.js
+++ public/project/client/views/home/home.controller.js
@@ -2,25 +2,25 @@
     angular
         .module("OneWorldCareApp")
         .controller("HomeController", HomeController);
-		
-		
- function HomeController($scope){
-   var model = this;
-  model.myInterval = 1600;
-  model.slides = [
-    {
-      image: './images/Volunteer.jpg',
-      caption:'Be a Part of the Change, support those in the front line of the action!'
-    },
-    {
-      image: './images/Environment.jpg',
-      caption: 'For people wanting to make a difference.. affect change....'
-    },
-     {
-      image: './images/Animal-Welfare.jpg',
-      caption: 'We share this world with these amazing beings!'
+
+
+    function HomeController($scope){
+        var model = this;
+        model.myInterval = 1600;
+        model.slides = [
+            {
+                image: './images/Animal-Welfare.jpg',
+                caption:'We share this world with these amazing beings!'
+            },
+            {
+                image: './images/Environment.jpg',
+                caption: 'For people wanting to make a difference.. affect change....'
+            },
+            {
+                image: './images/Nature.jpg',
+                caption: 'Be a Part of the Change, the earth needs caretakers!'
+            }
+        ];
     }
-   ];
-}
- })();
+})();
 
